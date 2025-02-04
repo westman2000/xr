be678209e1e16737bcc87d8a57005c4f8109b953
Mykola Nikolayev <mnikolayev@magicleap.com>
2025-02-04 16:02:40 -0500

    sync gradle files, project setup, launcher icon, manifest with sample: https://github.com/android/xr-samples

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# xr
-Android XR experiments
+# XRExp
+
+This repository contains an Android Studio project that provides a straightforward example of the
+basic functionality afforded to Android apps in Android XR.
+
+For more information, please [read the documentation](https://developer.android.com/develop/xr).
+
+# Features
+
+In the sample you can see an implementation of:
+
+- Spatial Panels
+- Orbiters
+- Environments
+- and more
+
+# 💻 Development Environment
+
+**XRExp** uses the Gradle build system and can be imported directly into Android Studio
+(make sure you are using the latest stable version available
+[here](https://developer.android.com/studio/preview)).
+
+# Additional Resources
+
+- https://developer.android.com/xr
+- https://developer.android.com/develop/xr
+- https://developer.android.com/design/ui/xr
+- https://developer.android.com/develop/xr#bootcamp
+
+# License
+
+**XRExp** is distributed under the terms of the Apache License (Version 2.0). See the
+[license](LICENSE) for more information.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,19 @@ android {
     buildFeatures {
         compose = true
     }
+    @Suppress("UnstableApiUsage")
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.4"
+    }
+    @Suppress("UnstableApiUsage")
+    experimentalProperties["android.experimental.enableScreenshotTest"] = true
 }
 
 dependencies {
+
+    val composeBom = platform(libs.androidx.compose.bom)
+    implementation(composeBom)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -47,19 +57,25 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.runtime)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.compose)
-    implementation(libs.runtime)
-    implementation(libs.androidx.scenecore)
+
+    // XR
+    implementation(libs.androidx.xr.compose)
+    implementation(libs.androidx.xr.runtime)
+    implementation(libs.androidx.xr.scenecore)
+    implementation(libs.androidx.xr.arcore)
+
+    // Testing
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(composeBom)
     androidTestImplementation(libs.androidx.ui.test.junit4)
+
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
