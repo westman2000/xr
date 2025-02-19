8e2cbbca20043a000db77e30635ea5d4429de4ac
Mykola Nikolayev <mnikolayev@magicleap.com>
2025-02-19 09:23:43 -0500

    libs cleanup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,18 +61,14 @@ dependencies {
     implementation(libs.androidx.concurrent.futures)
 
     // material icons
-    implementation("androidx.compose.material:material-icons-core:1.7.7")
+    implementation(libs.androidx.material.icons.core)
     // material3
-    implementation(libs.androidx.material3)
+//    implementation(libs.androidx.material3)
     // material3 Adaptive (CAMAL) dependencies
-
-    implementation("androidx.compose.material3.adaptive:adaptive:1.1.0-beta01")
-    implementation("androidx.compose.material3.adaptive:adaptive-layout:1.1.0-beta01")
-    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.1.0-beta01")
-    implementation("androidx.compose.material3.adaptive:adaptive-navigation:1.1.0-beta01")
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite-android:1.4.0-alpha07")
-
-
+    implementation(libs.androidx.adaptive)
+    implementation(libs.androidx.adaptive.layout)
+    implementation(libs.androidx.adaptive.navigation)
+    implementation(libs.androidx.material3.adaptive.navigation.suite.android)
 
     // media3
     implementation(libs.androidx.media3.exoplayer)
